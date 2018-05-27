e67c4f8d8a5d71a52e3fb82899f97ca2e35c3147
zubin.pratap@gmail.com <zubin.pratap@gmail.com>
2018-05-27 07:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,25 @@ function logShout(string) {
   console.log(string.toUpperCase());
 }
 
+function logWhisper(string){
+  console.log(string.toLowerCase);
+}
 
 
 
 
 
 
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
 
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
 
 
